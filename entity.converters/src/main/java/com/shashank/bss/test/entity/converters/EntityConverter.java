9b205ede2eaab2ca3575224bc46fde4e2e2d3f94
Shashank Makkar <shashank.makkar@ericsson.com>
2016-12-31 15:48:56 +0530

    Attempt to fix bean not found error

--- entity.converters/src/main/java/com/shashank/bss/test/entity/converters/EntityConverter.java
+++ entity.converters/src/main/java/com/shashank/bss/test/entity/converters/EntityConverter.java
@@ -5,4 +5,10 @@ import org.springframework.stereotype.Service;
 @Service ("entityConveters")
 public class EntityConverter
 {
+    @Override public String toString()
+    {
+        final StringBuilder sb = new StringBuilder("Hello EntityConverter{");
+        sb.append('}');
+        return sb.toString();
+    }
 }
